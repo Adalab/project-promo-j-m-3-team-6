69cb034df38ac123ea7cd78f37c8adce07812e18
IsabelOlivaresR <isabel.olivares6@gmail.com>
2020-07-27 13:12:47 +0200

    add docs

---
+++ docs/precache-manifest.e6911e1d1cc9876707efaf6c68a49df4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5820132ab4295fa578bd57ca8a924db",
+    "url": "./index.html"
+  },
+  {
+    "revision": "654ac9d65c3e80141c46",
+    "url": "./static/css/main.adde6a2d.chunk.css"
+  },
+  {
+    "revision": "28c06bd088511c0209fd",
+    "url": "./static/js/2.ec08af08.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.ec08af08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "654ac9d65c3e80141c46",
+    "url": "./static/js/main.b22e1461.chunk.js"
+  },
+  {
+    "revision": "c7a7eabf18310ce9d3e1",
+    "url": "./static/js/runtime-main.0bc4a3e2.js"
+  },
+  {
+    "revision": "915dc9a4c81216f94b206d31d52890e5",
+    "url": "./static/media/Logo-Gryffincode.915dc9a4.png"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/logo-awesome-profile-cards.0118e311.svg"
+  }
+]);
