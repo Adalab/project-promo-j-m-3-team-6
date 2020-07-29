27447c0c635f77eb474041282e34cc2e6a1641a9
miriamschaefer <msuarezmelian@gmail.com>
2020-07-29 18:26:24 +0200

    finish object

--- src/components/Form/Fill.js
+++ src/components/Form/Fill.js
@@ -16,21 +16,21 @@ class Fill extends React.Component {
 
   render() {
     return (
-      <div className='fillOut'>
+      <div className="fillOut">
         <div
-          className='fillOut__title js-arrow'
+          className="fillOut__title js-arrow"
           id={this.props.id}
           onClick={this.clickHandlerChild}
         >
-          <div className='content__title'>
-            <i className='fa fa-keyboard-o' aria-hidden='true'></i>
-            <h2 className='text'>Rellena</h2>
+          <div className="content__title">
+            <i className="fa fa-keyboard-o" aria-hidden="true"></i>
+            <h2 className="text">Rellena</h2>
           </div>
           <i
             className={`fa fa-chevron-down js-arrowsTransform ${
               this.props.isOpen === this.props.id ? '' : 'transform'
             }`}
-            aria-hidden='true'
+            aria-hidden="true"
           ></i>
         </div>
         <div
@@ -38,105 +38,110 @@ class Fill extends React.Component {
             this.props.isOpen === this.props.id ? '' : 'hidden'
           }`}
         >
-          <div className='item'>
-            <label htmlFor='name'>
+          <div className="item">
+            <label htmlFor="name">
               Nombre completo
               <input
-                type='text'
-                className='js-input-name'
-                id='name'
-                name='name'
-                placeholder='Ej: Jon Nieve'
+                type="text"
+                className="js-input-name"
+                id="name"
+                name="name"
+                placeholder="Ej: Jon Nieve"
                 required
                 onChange={this.objectHandlerChild}
               />
             </label>
           </div>
-          <div className='item'>
-            <label htmlFor='job'>
+          <div className="item">
+            <label htmlFor="job">
               Puesto
               <input
-                type='text'
-                className='js-input-job'
-                id='job'
-                name='job'
-                placeholder='Ej: Night guard'
+                type="text"
+                className="js-input-job"
+                id="job"
+                name="job"
+                placeholder="Ej: Night guard"
+                onChange={this.objectHandlerChild}
                 required
               />
             </label>
           </div>
-          <div className='item'>
-            <label htmlFor='img-selector'>
+          <div className="item">
+            <label htmlFor="img-selector">
               Imagen de perfil
-              <div className='fillOut-image'>
-                <div className='action'>
+              <div className="fillOut-image">
+                <div className="action">
                   <button
-                    className='action__upload-btn js__profile-trigger'
-                    type='button'
+                    className="action__upload-btn js__profile-trigger"
+                    type="button"
                   >
                     Añadir imagen
                   </button>
                   <input
-                    type='file'
-                    name=''
-                    id='img-selector'
-                    className='action__hiddenField js__profile-upload-btn'
+                    type="file"
+                    name=""
+                    id="img-selector"
+                    className="action__hiddenField js__profile-upload-btn"
                   />
                 </div>
-                <div className='profile'>
-                  <div className='profile__preview js__profile-preview'></div>
+                <div className="profile">
+                  <div className="profile__preview js__profile-preview"></div>
                 </div>
               </div>
             </label>
           </div>
-          <div className='item'>
-            <label htmlFor='email'>
+          <div className="item">
+            <label htmlFor="email">
               Email
               <input
-                type='email'
-                className='js-input-mail'
-                id='email'
-                name='email'
-                placeholder='Ej: jon-nieve@gmail.com'
+                type="email"
+                className="js-input-mail"
+                id="email"
+                name="email"
+                placeholder="Ej: jon-nieve@gmail.com"
+                onChange={this.objectHandlerChild}
                 required
               />
             </label>
           </div>
-          <div className='item'>
-            <label htmlFor='phone'>
+          <div className="item">
+            <label htmlFor="phone">
               Teléfono
               <input
-                type='tel'
-                className='js-input-phone'
-                id='phone'
-                name='phone'
-                placeholder='Ej: 123456789'
-                pattern='[0-9]{9}'
+                type="tel"
+                className="js-input-phone"
+                id="phone"
+                name="phone"
+                placeholder="Ej: 123456789"
+                pattern="[0-9]{9}"
+                onChange={this.objectHandlerChild}
               />
             </label>
           </div>
-          <div className='item'>
-            <label htmlFor='linkedin'>
+          <div className="item">
+            <label htmlFor="linkedin">
               Linkedin
               <input
-                type='text'
-                className='js-input-linkedin'
-                id='linkedin'
-                name='linkedin'
-                placeholder='Ej: jon.nieve'
+                type="text"
+                className="js-input-linkedin"
+                id="linkedin"
+                name="linkedin"
+                placeholder="Ej: jon.nieve"
+                onChange={this.objectHandlerChild}
                 required
               />
             </label>
           </div>
-          <div className='item'>
-            <label htmlFor='github'>
+          <div className="item">
+            <label htmlFor="github">
               Github
               <input
-                type='text'
-                className='js-input-github'
-                id='github'
-                name='github'
-                placeholder='Ej: jon-nieve'
+                type="text"
+                className="js-input-github"
+                id="github"
+                name="github"
+                placeholder="Ej: jon-nieve"
+                onChange={this.objectHandlerChild}
                 required
               />
             </label>
